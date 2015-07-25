b0613f65a5f0dba290caefcd474272f5131e98b8
inai17ibar <inai17ibar@gmail.com>
2015-07-25 19:21:20 +0900

    It can get the number of ranking, the title name and image link from Pixiv site.

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # pixiv-filtering
+
+#### 概要
+
+
+#### 保存するindexファイルの構造
+
+*
+
+#### 取得する元のURLについて
+
+- ランキングページを使用する．
+	* http://www.pixiv.net/ranking.php
+	* 以下、細かいランキングページも存在する．
+
+```
+pixiv.text.today = '本日';
+pixiv.text.yesterday = '昨日';
+pixiv.text.notifications = 'メッセージ・ポップボード';
+
+pixiv.text.dailyRanking = 'デイリーランキング';
+pixiv.text.weeklyRanking = 'ウィークリーランキング';
+pixiv.text.monthlyRanking = 'マンスリーランキング';
+pixiv.text.rookieRanking = 'ルーキーランキング';
+pixiv.text.daily_r18Ranking = 'R-18 デイリーランキング';
+pixiv.text.r18gRanking = 'R-18G ランキング';
+pixiv.text.maleRanking = '男子に人気ランキング';
+pixiv.text.femaleRanking = '女子に人気ランキング';
+```
--- crawler.rb
+++ crawler.rb
@@ -1,11 +1,27 @@
 require 'rubygems'
-require 'nokogiri'
+# URLにアクセスするためのライブラリの読み込み
 require 'open-uri'
+# Nokogiriライブラリの読み込み
+require 'nokogiri'
 
-url = "http://news.yahoo.co.jp/"
-doc = Nokogiri::HTML(open(url))
+# スクレイピング先のURL
+url = 'http://www.pixiv.net/ranking.php'
 
-for i in 1..8 do
-	top_news_css = "li:nth-child(" + i.to_s + ") div"
-	puts doc.at_css(top_news_css).text
+charset = nil
+html = open(url) do |f|
+  charset = f.charset # 文字種別を取得
+  f.read # htmlを読み込んで変数htmlに渡す
+end
+
+# htmlをパース(解析)してオブジェクトを作成
+doc = Nokogiri::HTML.parse(html, nil, charset)
+
+doc.xpath('//section[@class="ranking-item"]').each do |node|
+  # title
+  p node.css('h1').inner_text
+  p node.css('h2').inner_text
+  # 記事のサムネイル画像
+  p node.css('img').attribute('data-src').value
+  # 記事のサムネイル画像
+  #p node.css('a').attribute('href').value
 end
